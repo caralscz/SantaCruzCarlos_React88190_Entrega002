44f3b1bb608dc7e7fa2760a850321212270f7715
Cscz <sczcaral@gmail.com>
2025-06-16 16:26:04 -0300

    Algo de estilo

--- src/components/Contactenos.jsx
+++ src/components/Contactenos.jsx
@@ -15,8 +15,14 @@ const Contactenos = () => {
             <h3>Pre-entrega 002 de Santa Cruz Carlos - Coder ReactJS</h3>
             <hr />
             <p>c_santacruz@hotmail.com<br />
-            TE  00 00 123 456789<br />
-            coderFlex React Js Flex</p>
+                TE  00 00 123 456789<br /></p>
+            <hr />
+            <p>Comisión 88190 Del 13/05/25 al 01/07/25<br />
+                Martes  19:00 a 21:00h<br />
+                Profe: Laura Therisod<br />
+                Tutor: Andres Rubio<br />
+                coderFlex React Js Flex</p>
+
 
         </>
     );
--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -14,8 +14,14 @@ const ErrorPage = () => {
     <>
       <h1 className="titulo-principal" >***  Error 404***</h1>
       <h3>Lo siento, la pagina no existe !! </h3>
-      <Link to='/'  className="btn btn-secondary">Regresar al inicio</Link>
+      <Link to='/' className="btn btn-secondary">Regresar al inicio</Link>
       <hr />
+      <p>Comisión 88190 Del 13/05/25 al 01/07/25<br />
+        Martes  19:00 a 21:00h<br />
+        Profe: Laura Therisod<br />
+        Tutor: Andres Rubio<br />
+        coderFlex React Js Flex</p>
+
     </>
   )
 }
